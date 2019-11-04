216bba8dea925f37492ec087528856eaa8e9581f
joshxcho <joshxcho123@gmail.com>
2019-11-04 00:28:31 -0800

    Fix className

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,7 +3,7 @@ import React from "react";
 const Search = ({ placeholderText, searchValue, updateSearchValue }) => (
   <section>
     <input
-      className="searchBar"
+      className="searchInput"
       type="search"
       placeholder={placeholderText}
       onChange={updateSearchValue}
